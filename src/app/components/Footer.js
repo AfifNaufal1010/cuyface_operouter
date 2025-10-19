6ac270a53618cf52703d887638338d32a5f00ff4
AfifNaufal1010 <mohd.afifnaufal@student.uir.ac.id>
2025-10-19 11:41:00 +0700

    initial setup

---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <p>&copy; CuyFace. Ramalan ini berdasarkan model AI vision, bukan untuk di percaya sepenuhnya.</p>
+    </footer>
+  )
+}
+
+export default Footer
